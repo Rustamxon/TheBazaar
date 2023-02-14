ff021603a0ef483ba3205b3926aeaef0703f2ebe
Axmadjonov Abdulloh <abdulloh.7013@gmail.com>
2023-02-14 15:36:40 +0500

    Added QuestionService

--- TheBazaar.Service/Interfaces/IQuestionService.cs
+++ TheBazaar.Service/Interfaces/IQuestionService.cs
@@ -8,7 +8,7 @@ public interface IQuestionService
 {
     Task<GenericResponse<Question>> CreateAsync(QuestionDto question);
     Task<GenericResponse<Question>> DeleteAsync(long id);
-    Task<GenericResponse<Question>> UpdateAsync(long id, QuestionDto question);
+    Task<GenericResponse<Question>> UpdateAsync(Question question);
     Task<GenericResponse<Question>> GetAsync(long id);
     Task<GenericResponse<List<Question>>> GetAllForAdminAsync(long userId);
 }
---
+++ TheBazaar.Service/Services/QuestionService.cs
@@ -0,0 +1,106 @@
+﻿using TheBazaar.Data.IRepositories;
+using TheBazaar.Data.Repositories;
+using TheBazaar.Domain.Entities;
+using TheBazaar.Domain.Enums;
+using TheBazaar.Service.DTOs;
+using TheBazaar.Service.Helpers;
+using TheBazaar.Service.Interfaces;
+
+namespace TheBazaar.Service.Services;
+
+public class QuestionService : IQuestionService
+{
+    private IGenericRepo<Question> questionRepo = new GenericRepo<Question>();
+    public async Task<GenericResponse<Question>> CreateAsync(QuestionDto question)
+    {
+        var que = new Question
+        {
+            UserId = question.UserId,
+            QuestionText = question.QuestionText,
+            Progress = QuestionProgressType.Pending,
+            CreatedAt = DateTime.Now
+        };
+
+        var res = await questionRepo.CreateAsync(que);
+
+        return new GenericResponse<Question>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Value = res
+        };
+    }
+
+    public async Task<GenericResponse<Question>> DeleteAsync(long id)
+    {
+        var res = await questionRepo.DeleteAsync(id);
+
+        if (res == false)
+            return new GenericResponse<Question>
+            {
+                StatusCode = 404,
+                Message = "Not found",
+                Value = null
+            };
+
+        return new GenericResponse<Question>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Value = null
+        };
+    }
+
+    public async Task<GenericResponse<List<Question>>> GetAllForAdminAsync(long userId)
+    {
+        var result = (await questionRepo.GetAllAsync()).FindAll(u => u.Progress == QuestionProgressType.Pending);
+
+        return new GenericResponse<List<Question>>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Value = result
+        };
+    }
+
+    public async Task<GenericResponse<Question>> GetAsync(long id)
+    {
+        var result = await questionRepo.GetAsync(id);
+
+        if (result is null)
+            return new GenericResponse<Question>
+            {
+                StatusCode = 404,
+                Message = "Success",
+                Value = null
+            };
+        
+        return new GenericResponse<Question>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Value = result
+        };
+    }
+
+    public async Task<GenericResponse<Question>> UpdateAsync(Question question)
+    {
+        question.UpdatedAt = DateTime.Now;
+        var res = await questionRepo.UpdateAsync(question);
+
+        if (res is null)
+            return new GenericResponse<Question>
+            {
+                StatusCode = 400,
+                Message = "Something went wrong",
+                Value = null
+            };
+
+        return new GenericResponse<Question>
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Value = res
+        };
+    }
+}
