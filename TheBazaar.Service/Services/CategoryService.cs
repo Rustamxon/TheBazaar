64ad2c46c1143287a5c4ed164c0df9f023b3c77f
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-02-14 14:46:53 +0500

    CategoryService few written

---
+++ TheBazaar.Service/Services/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBazaar.Data.IRepositories;
+using TheBazaar.Domain.Entities;
+using TheBazaar.Service.DTOs;
+using TheBazaar.Service.Helpers;
+using TheBazaar.Service.Interfaces;
+
+namespace TheBazaar.Service.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private IGenericRepo
+        public Task<GenericResponse<Category>> CreateAsync(CategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponse<Category>> DeleteAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponse<List<Category>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponse<Category>> GetAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GenericResponse<Category>> UpdateAsync(string name, CategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
