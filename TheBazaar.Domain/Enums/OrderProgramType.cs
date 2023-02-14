111703703f21490ff8441cf03ec20a954e2ccb76
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-02-14 09:56:17 +0500

    Added enums

---
+++ TheBazaar.Domain/Enums/OrderProgramType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBazaar.Domain.Enums
+{
+    public enum OrderProgramType : byte
+    {
+        Pending = 1,
+        Processing = 5,
+        Shipped = 10,
+        Delivered = 15,
+        Cancelled = 20,
+        Returned = 25,
+        Complete = 30,
+    }
+}
