9e52bf523f2a2067ca17c8aa1980d3e3b3c735cc
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-02-14 10:12:44 +0500

    Added product and category

--- TheBazaar.Domain/Entities/Product.cs
+++ TheBazaar.Domain/Entities/Product.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheBazaar.Domain.Commons;
 
 namespace TheBazaar.Domain.Entities
 {
-    internal class Product
+    public class Product : Auditable
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] SearchTags { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+        public long CategoryId { get; set; }
     }
 }
