30b162a3e524752d6b397b8b5220fdc37ebcd820
Axmadjonov Abdulloh <abdulloh.7013@gmail.com>
2023-02-14 10:48:55 +0500

    Added json files and paths

---
+++ TheBazaar.Data/Configurations/DatabasePaths.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBazaar.Data.Configurations;
+
+public static class DatabasePaths
+{
+    public const string CATEGORY_PATH = "..\\..\\..\\..\\TheBazaar.Data\\Databases\\category.json";
+    public const string ORDER_PATH = "..\\..\\..\\..\\TheBazaar.Data\\Databases\\order.json";
+    public const string PRODUCT_PATH = "..\\..\\..\\..\\TheBazaar.Data\\Databases\\product.json";
+    public const string QUESTION_PATH = "..\\..\\..\\..\\TheBazaar.Data\\Databases\\question.json";
+    public const string USER_PATH = "..\\..\\..\\..\\TheBazaar.Data\\Databases\\user.json";
+}
