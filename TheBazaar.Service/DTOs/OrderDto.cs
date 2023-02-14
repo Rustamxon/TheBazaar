33685ff711220e3ff1d8c54497c203b4502d6669
KomronSharpist <komrondeveloper@gmail.com>
2023-02-14 18:22:17 +0500

    Created OrderService

--- TheBazaar.Service/DTOs/OrderDto.cs
+++ TheBazaar.Service/DTOs/OrderDto.cs
@@ -1,11 +1,13 @@
-﻿using TheBazaar.Domain.Enums;
+﻿using TheBazaar.Domain.Entities;
+using TheBazaar.Domain.Enums;
 
 namespace TheBazaar.Service.DTOs;
 
 public class OrderDto
 {
-    public List<ProductDto> Items { get; set; }
+    public List<Product> Items { get; set; }
     public long UserId { get; set; }
     public string Address { get; set; }
     public PaymentType PaymentType { get; set; }
+    public OrderProgressType Progress { get; set; }
 }
